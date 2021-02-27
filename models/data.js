58b048cff0d8535b0bc29c4b043bacd8da7960fc
langjeff <lang.jeffrey@gmail.com>
2021-02-27 09:47:45 -0700

    setup file structure for application

---
+++ models/data.js
@@ -0,0 +1,15 @@
+//* needs to be updates for our model
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
