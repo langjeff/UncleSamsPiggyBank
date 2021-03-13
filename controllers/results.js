29c4d93e3ef60df0809f8c6968a567b2959a0c94
Jon Godwin <45859095+JonDnv@users.noreply.github.com>
2021-03-13 12:54:12 -0700

    results controller

---
+++ controllers/results.js
@@ -0,0 +1,15 @@
+const db = require("../models");
+
+// Defining methods for the userController
+module.exports = {
+  findAll: function (req, res) {
+    db.Results.find(req.query)
+
+      .then((results) => res.json(results))
+      .catch((err) => res.status(422).json(err));
+    },
+    
+    saveResults: function (req, res) {
+        db.Results.
+    }
+};
