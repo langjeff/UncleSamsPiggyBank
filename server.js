99d1298b2ae809744ed0eb7fd91433fda710d769
langjeff <lang.jeffrey@gmail.com>
2021-03-16 20:06:00 -0600

    fixed ben's fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build")));
+  app.use(express.static("client/build")));
 }
 
 // Connect to the Mongo DB
