ef7a7e27e8533d979855207f5ea972b28825c049
langjeff <lang.jeffrey@gmail.com>
2021-03-16 19:12:20 -0600

    moved routes down

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
+
+// Add routes, both API and view
+app.use(routes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build/static")));
 }
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/unclesam");
